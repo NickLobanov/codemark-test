c4c9562fc87cef05fd03ad7e55e5895e081fb936
Nick Lobanov <nk.nicolas142@gmail.com>
2020-12-21 18:38:05 +0300

    fix sort cards

--- src/components/SortCard/SortCard.js
+++ src/components/SortCard/SortCard.js
@@ -4,12 +4,13 @@ import Card from '../Card/Card';
 
 function SortCard({ cards, tag, cardClick}) {
 
-    console.log(cards)
+    const sortCards = cards.filter(data => data.keyword === tag)
+
     return (
         <section className="sort">
             <h2 className="sort__title">{tag}</h2>
             <div className="sort__wrap">
-                {cards.map(data => (
+                {sortCards.map(data => (
                     <Card key={data.keyword}
                         img={data.img}
                         alt={data.keyword}
